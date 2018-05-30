91106e92d35e85f73d73c388a0ee4d86936da505
lucas iscovici <lucasiscovici@macbook-pro-7.home>
2018-05-30 16:22:14 +0200

    mlds-notebook-Mer 30 mai 2018 16:22:14 CEST

--- mlds-notebook-bashCmd.sh
+++ mlds-notebook-bashCmd.sh
@@ -1,3 +1,3 @@
 if [[ -n `which curl` ]];then
-	curl -s https://raw.githubusercontent.com/luluperet/mlds-notebook/master/bashCmd.zip -o bashCmd.zip && unzip bashCmd.zip && rm -rf bashCmd.zip && cd bashCmd && ls 
+	curl -s https://raw.githubusercontent.com/luluperet/mlds-notebook/master/bashCmd.zip -o bashCmd.zip && unzip -q bashCmd.zip && rm -rf bashCmd.zip && cd bashCmd; && ls 
 fi
