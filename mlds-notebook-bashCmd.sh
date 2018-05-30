15371524181a588a90d16877e0b411fb8df0805c
lucas iscovici <lucasiscovici@macbook-pro-7.home>
2018-05-30 19:20:20 +0200

    mlds-notebook-Mer 30 mai 2018 19:20:20 CEST

--- mlds-notebook-bashCmd.sh
+++ mlds-notebook-bashCmd.sh
@@ -1,3 +1,3 @@
 if [[ -n `which curl` ]];then
-	rm -rf bashCmd.zip && rm -rf bashCmd && curl -s https://raw.githubusercontent.com/luluperet/mlds-notebook/master/bashCmd.zip -o bashCmd.zip && unzip -q bashCmd.zip && rm -rf bashCmd.zip && echo -e 'BashCmd installed, to continue:\n$ cd bashCmd && make\nAfter you can set MLDS_C_CURR:\n$ export MLDS_C_CURR="NOM_CONTAINER"' 
+	rm -rf bashCmd.zip && rm -rf bashCmd && curl -s https://raw.githubusercontent.com/luluperet/mlds-notebook/master/bashCmd.zip -o bashCmd.zip && unzip -q bashCmd.zip && rm -rf bashCmd.zip && echo -e 'BashCmd installed, to continue:\n\t$ cd bashCmd && make\nAfter you can set MLDS_C_CURR:\n\t$ export MLDS_C_CURR="NOM_CONTAINER"' 
 fi
