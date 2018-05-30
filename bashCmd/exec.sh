1a79005e5f3eca2a35e0b9d9f0bc197b53cde025
lucas iscovici <lucasiscovici@macbook-pro-7.home>
2018-05-30 19:16:54 +0200

    mlds-notebook-Mer 30 mai 2018 19:16:54 CEST

--- bashCmd/exec.sh
+++ bashCmd/exec.sh
@@ -1,14 +1,35 @@
 
 function _exec(){
-	docker exec -ti "$1" bash -c '$2'
+	if [[ $(docker ps --format="{{.Names}}" | grep -q "$1") ]] && [[ $(docker ps -q | grep -q "$1") ]]; then
+		echo "PB $1 n'existe pas"
+	else
+		# echo -e "PB $1 existe\n"
+		# echo -e docker exec $3 "$1" bash -c "$2"
+		if [[ -z $3 ]]; then
+			docker exec "$1" bash -c "$2"
+		else 
+			docker exec $3 "$1" bash -c "$2"
+		fi
+	fi
 }
 
-if [[ $# -lt 2 ]]; then
-	if [[ -z $MLDS_C_CURR ]]; then
-		echo "exec.sh container_ID CMD"
+if [[ $# -lt 3 ]]; then
+	if [[ $# -eq 2 ]]; then
+		if [[ $1 == "ti" || $1=="it" || $1=="-ti"  || $1=="-it" ]]; then
+			shift
+			if [[ -z $MLDS_C_CURR ]]; then
+				echo -e "exec.sh container_ID CMD"
+			else
+			   _exec "$MLDS_C_CURR" $1 "-ti"
+			fi
+		fi
 	else
-		docker exec -ti "$MLDS_C_CURR" bash -c '$2'
+		if [[ -z $MLDS_C_CURR ]]; then
+			echo -e "exec.sh container_ID CMD"
+		else
+			 _exec "$MLDS_C_CURR" "$1"
+		fi
 	fi
 else
-	docker exec -ti "$1" bash -c '$2'
+	 _exec "$1" "$2" "$3"
 fi
---
+++ bashCmd/open.sh
@@ -0,0 +1,5 @@
+if [[ $1 == "nb" ]]; then
+	open http://localhost:8888
+else
+	open http://localhost:$1
+fi
