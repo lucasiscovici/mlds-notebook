ffa4369b7c2aad18aec7bdcb9d87139d422ce092
lucas iscovici <lucasiscovici@macbook-pro-7.home>
2018-05-30 16:35:13 +0200

    mlds-notebook-Mer 30 mai 2018 16:35:13 CEST

--- mlds-notebook-bashCmd.sh
+++ mlds-notebook-bashCmd.sh
@@ -1,3 +1,3 @@
 if [[ -n `which curl` ]];then
-	curl -s https://raw.githubusercontent.com/luluperet/mlds-notebook/master/bashCmd.zip -o bashCmd.zip && unzip -q bashCmd.zip && rm -rf bashCmd.zip && echo -n "BashCmd installed, to continue:\n$ cd bashCmd && make  " 
+	curl -s https://raw.githubusercontent.com/luluperet/mlds-notebook/master/bashCmd.zip -o bashCmd.zip && unzip -q bashCmd.zip && rm -rf bashCmd.zip && echo -n 'BashCmd installed, to continue:\n$ cd bashCmd && make\n' 
 fi
