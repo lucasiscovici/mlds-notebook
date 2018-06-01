79c0bc50e0fd61a23e23135ab38710a1909586e5
lucas iscovici <lucasiscovici@macbook-pro-7.home>
2018-06-02 01:32:07 +0200

    mlds-notebook-Sam  2 jui 2018 01:32:07 CEST

--- mlds-notebook-bashCmd.sh
+++ mlds-notebook-bashCmd.sh
@@ -1,3 +1,3 @@
 if [[ -n `which curl` ]];then
-	rm -rf bashCmd.zip && rm -rf bashCmd && curl -s https://raw.githubusercontent.com/luluperet/mlds-notebook/master/bashCmd.zip -o bashCmd.zip && unzip -q bashCmd.zip && rm -rf bashCmd.zip && echo -e 'BashCmd installed, to continue:\n\t$ cd bashCmd \nmake or make help or make mlds help= For see help\nFor begin:\n\t$ ./setEnv.sh <name_futur_container_u_want>"' 
+	rm -rf bashCmd.zip && rm -rf bashCmd && curl -s https://raw.githubusercontent.com/luluperet/mlds-notebook/master/bashCmd.zip -o bashCmd.zip && unzip -q bashCmd.zip && rm -rf bashCmd.zip && echo -e 'BashCmd installed, to continue:\n\t$ cd bashCmd \nFor see help:\n\t$ make or $ make help or $ make mlds help=\nFor begin:\n\t$ ./setEnv.sh <name_futur_container_u_want>"' 
 fi
