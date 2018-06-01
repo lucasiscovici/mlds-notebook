befa1d562e6e7709c49b10d1a26454f1654b1821
lucas iscovici <lucasiscovici@macbook-pro-7.home>
2018-06-02 01:20:01 +0200

    mlds-notebook-Sam  2 jui 2018 01:20:01 CEST

--- bashCmd/_check.sh
+++ bashCmd/_check.sh
@@ -1,1 +1,1 @@
-$(docker ps --format="{{.Names}}" | grep -q "$1") || $(docker ps -q | grep -q "$1")
+$(docker ps --format="{{.Names}}" | grep -q "^$1$") || $(docker ps -q | grep -q "^$1$")
--- bashCmd/envStart.sh
+++ bashCmd/envStart.sh
@@ -14,9 +14,10 @@ else
 		if ./_check.sh "$1"; then
 			curr="*"
 		fi
+		export PATH="$(pwd):$PATH";
 		export OLD_PS1="SETTED";
 		export PS1="MLDS-NB-C-CURR->$1$curr):\W\$ " ;
 		export MLDS_C_CURR="$1" ;
-bash --rcfile <(echo "function check(){ export PS1=\"MLDS-NB-C-CURR->$MLDS_C_CURR\$(./_check.sh $MLDS_C_CURR && echo '*')):\W$ \"; }; trap 'check' USR1; echo -e \"For Stop The Env\n\t$ exit$\";") && echo "MLDS Env \"$1\" Stopped"
+bash --rcfile <(echo "function check(){ export PS1=\"MLDS-NB-C-CURR->$MLDS_C_CURR\$(./_check.sh $MLDS_C_CURR && echo '*')):\W$ \"; }; trap 'check' USR1; echo -e \"For Stop The Env\n\t$ exit\";export pidMldsBase=\$$")
 	fi
 fi
