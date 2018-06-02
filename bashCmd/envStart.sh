e6cc81b8838a0ef7710674c7dd961bc69056d722
lucas iscovici <lucasiscovici@macbook-pro-7.home>
2018-06-02 05:06:39 +0200

    mlds-notebook-Sam  2 jui 2018 05:06:39 CEST

--- bashCmd/envStart.sh
+++ bashCmd/envStart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-if [[ -z "$1" ]]; then
+
+
+sl="$1"
+if [[ -z "$sl" ]]; then
 	if [[ -n "$OLD_PS1" ]]; then
 		echo -e "For Stop The Env\n$ exit"
 	fi
@@ -18,11 +21,11 @@ else
 		export _dockerP=$(which docker)
 		export PATH="$(pwd):$PATH";
 		export OLD_PS1="SETTED";
-		if ./_check.sh "$1"; then
+		if ./_check.sh "$sl"; then
 			curr="*"
 		fi
-		export PS1="MLDS-NB-C-CURR->$1$curr):\W\$ " ;
-		export MLDS_C_CURR="$1" ;
-bash --rcfile <(echo "trap \"./_gb.sh\" exit;shopt -s expand_aliases;alias exit=\"./_exit.sh && _exit\";alias _exit=\"builtin exit\"; function _docker(){ "$_dockerP" \$@;};export -f _docker;function exit(){ ./_exit.sh 0; };function check(){ export PS1=\"MLDS-NB-C-CURR->$MLDS_C_CURR\$(./_check.sh $MLDS_C_CURR && echo '*')):\W$ \"; }; trap 'check' USR1; echo -e \"For Stop The Env\n\t$ exit\";export pidMldsBase=\$$;") || ./_gb.sh
+		export PS1="MLDS-NB-C-CURR->$sl$curr):\W\$ " ;
+		export MLDS_C_CURR="$sl" ;
+bash --rcfile <(echo "function chbash(){ curr="";if ./_check.sh "\$1"; then curr="*"; fi; export PS1=\"MLDS-NB-C-CURR->\$1\$curr):\W\$ \";export MLDS_C_CURR=\"\$1\" ; };trap \"./_gb.sh\" exit;shopt -s expand_aliases;alias exit=\"./_exit.sh && _exit\";alias _exit=\"builtin exit\"; function _docker(){ "$_dockerP" \$@;};export -f _docker;function exit(){ ./_exit.sh 0; };function check(){ export PS1=\"MLDS-NB-C-CURR->\$MLDS_C_CURR\$(./_check.sh \$MLDS_C_CURR && echo '*')):\W$ \"; }; trap 'check' USR1;trap 'chbash \$(cat ./.tmpChangeEnv) && rm -rf ./.tmpChangeEnv;' USR2; echo -e \"For Stop The Env\n\t$ exit\";export pidMldsBase=\$$;") || ./_gb.sh
 	fi
 fi
