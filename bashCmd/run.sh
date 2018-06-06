ad1a3736ae8ce360b0f86732fb9714c00eaf214a
lucas iscovici <lucasiscovici@macbook-pro-7.home>
2018-06-06 22:19:20 +0200

    mlds-notebook-Mer  6 jui 2018 22:19:20 CEST

--- bashCmd/run.sh
+++ bashCmd/run.sh
@@ -1,10 +1,10 @@
 if [[ -n "$OLD_PS1" ]]; then
 	#statements
-dkg="$(./confImgRun.sh)"
+dkd="$(./confImgRun.sh)"
 # echo "DEBUG $dkd"
 pdof=\"$(cat ../.mldsEnv | sed -En "/^__.*/p" | sed -En "s/__(.*)/\1/p"  | tr '\n' ' ')\"
 
-# echo make mlds $(echo $dkd | tr -d '"' ) $pdof  $@
+#echo make mlds $(echo $dkd | tr -d '"' ) $(echo $pdof | tr -d '"' )    $@
 make mlds $(echo $dkd | tr -d '"' ) $(echo $pdof | tr -d '"' )  $@
 
 else
